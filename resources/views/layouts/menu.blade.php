debf939484499c4093106767daa48364168736a6
Paisa2 <villca.fuente23@gmail.com>
2024-08-09 14:04:27 -0400

    completando el crud de usuarios y roles

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,6 +1,6 @@
 <li class="side-menus {{ Request::is('*') ? 'active' : '' }}">
-    <a class="nav-link" href="#">
-        <i class=" fas fa-building"></i><span>Colcapirhua</span>
+    <a class="nav-link" href="/home">
+        <i class=" fas fa-building"></i><span>Dashboard</span>
     </a>
     <a class="nav-link" href="/usuarios">
         <i class=" fas fa-users"></i><span>Usuarios</span>
@@ -10,5 +10,14 @@
     </a>
     <a class="nav-link" href="/blogs">
         <i class="fas fa-blog"></i><span> Blogs</span>
+    </a>
+    <a class="nav-link" href="/profesores">
+        <i class="fas fa-chalkboard-teacher"></i><span> Profesores</span>
+    </a>
+    <a class="nav-link" href="/alunnos">
+        <i class="fas fa-user-friends"></i><span> Alunnos</span>
+    </a>
+    <a class="nav-link" href="/cursos">
+        <i class="fas fa-store-alt"></i><span> Cursos</span>
     </a>
 </li>
